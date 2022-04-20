7482c1f1dd1caf64d9b4c270bc79766df3a8b670
Tushar2909 <tusharchavan2929@gmail.com>
2022-04-21 00:17:14 +0530

    enter

--- models/donor.js
+++ models/donor.js
@@ -1,27 +1,32 @@
 module.exports = (sequelize, DataTypes) => {
-    return sequelize.define("donor", {
-      mobileno: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      foodDetail: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      address: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      userlocation: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
+  return sequelize.define("donor", {
+    mobileno: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    foodDetail: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    latitude: {
+      type: DataTypes.DOUBLE,
+      allowNull: true,
+    },
+    longitude: {
+      type: DataTypes.DOUBLE,
+      allowNull: true,
+    },
 
-    }, {
-        timestamps: false,
-    });
+
+  }, {
+    timestamps: false,
+  });
 };
--- routes/index.js
+++ routes/index.js
@@ -14,20 +14,25 @@ router.post('/users', async (req, res) => {
     var user = await db.user.create(userData);
     res.send(user);
 });
+router.post('/donors', async (req, res) => {
+    var donorData = req.body;
+    var donor = await db.donor.create(donorData);
+    res.send(donor);
+});
 
 router.get('/users/:number', async (req, res) => {
     // var userData = req.body;
     var num = req.params.number;
     console.log(num);
-    var user = await db.user.findAll({where:{number: num},},);
+    var user = await db.user.findAll({ where: { number: num }, },);
     console.log(user);
     res.send(user);
 });
 
 router.post('/isValid', async (req, res) => {
     var mobileno = req.body["mobileno"];
-    var count = await db.user.count({where: {number: mobileno}});
-    if(count <= 0) {
+    var count = await db.user.count({ where: { number: mobileno } });
+    if (count <= 0) {
         res.send(false);
     } else {
         res.send(true);
@@ -37,10 +42,6 @@ router.get("/donors", async (req, res) => {
     var donors = await db.donor.findAll();
     res.send(donors);
 });
-router.post('/donors', async (req, res) => {
-    var donorData = req.body;
-    var donor = await db.donor.create(donorData);
-    res.send(donor);
-});
+
 
 module.exports = router;
